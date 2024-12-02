e5a74ac231c28662db82f4d7f8da8508004403b5
Taisuke Fukuno <fukuno@jig.jp>
2024-12-02 21:13:07 +0900

    add ユーザー情報, pos as the route

--- make2.js
+++ make2.js
@@ -17,11 +17,15 @@ for (const fn of fns) {
 
   const user = fn.substring(0, fn.length - 4);
   const cnt = data.length;
-  const pos = ArrayUtil.toUnique(data.map(i => i.施設No));
+  //const pos = ArrayUtil.toUnique(data.map(i => i.施設No));
+  const pos = data.map(i => i.施設No);
   console.log(user, cnt, pos.length, pos);
   list.push({ user, cnt, poslen: pos.length, pos: pos.join(",") });
 
-  data.forEach(i => all.push(i));
+  data.forEach(i => {
+    i.ユーザー情報 = user;
+    all.push(i);
+  });
 }
 list.sort((a, b) => (b.poslen - a.poslen) * 10000 + (b.cnt - a.cnt));
 await Deno.writeTextFile("user.csv", CSV.stringify(list, null, 2));
