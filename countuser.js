49195aa57cedb5e5a04799c7ea2d6bab1c9f4ee1
Taisuke Fukuno <fukuno@jig.jp>
2024-12-12 09:08:28 +0900

    add countuser

--- countuser.js
+++ countuser.js
@@ -3,20 +3,13 @@ import { dir2array } from "https://js.sabae.cc/dir2array.js";
 import { shrinkData } from "./shrinkData.js";
 
 const data = await CSV.fetchJSON("00_M5_facility list.csv");
-console.log(data, data.length);
-
-const dt = 30 * 60; // 30min if shrink
-//const dt = 0;
-
-await Deno.mkdir("user");
-
-// 9270655
-
-let cnt = 0;
 // No.,エリア,業種,施設名(日本語),施設名(ローマ字),緯度,経度
 //for (const item of data) {
 console.log(data.length);
 //Deno.exit();
+
+const dt = 30 * 60; // 30min if shrink
+//const dt = 0;
 
 // 38 ng
 // 41
@@ -26,17 +19,9 @@ console.log(data.length);
 
 //const nglist = [38, 41, 45, 51, 52, 53];
 
+const listall = {};
+let cnt = 0;
 const files = [];
-
-const writeUser = async (list) => {
-  for (const user in list) {
-    const data = list[user];
-    const path = "user/" + user + ".csv";
-    const data0 = await CSV.fetchJSON(path, []);
-    data0.forEach(i => data.push(i));
-    await Deno.writeTextFile(path, CSV.stringify(data));
-  }
-};
 
 for (let i = 0; i < data.length; i++) {
 //for (let i = 0; i < 30; i++) {
@@ -49,17 +34,17 @@ for (let i = 0; i < data.length; i++) {
   console.log(fn);
   const dir = await dir2array(fn);
   //console.log(dir);
-  const list = {};
   for (const f of dir) {
+    const list = {};
     const data0 = await CSV.fetchJSON(fn + "/" + f);
     const data = dt == 0 ? data0 : shrinkData(data0, dt);
     for (const item of data) {
       const user = item["ユーザー情報"];
       if (!list[user]) {
-        list[user] = [];       
+        list[user] = 0;
       }
       delete item["ユーザー情報"];
-      list[user].push(item);
+      list[user]++;
     }
     const listlen = Object.keys(list).length;
     cnt += data.length;
@@ -68,16 +53,24 @@ for (let i = 0; i < data.length; i++) {
       no,
       file: f,
       n_records: data0.length,
+      n_records_shrink: data.length,
       n_users: listlen,
     };
     files.push(file);
     console.log(Object.values(file).join(", "));
 
-    //
-    //await writeUser(list);
+    for (const user in list) {
+      if (!listall[user]) {
+        listall[user] = 0;
+      }
+      listall[user] += list[user];
+    }
   }
-  await writeUser(list);
+  // await writeUser(list);
 }
 //console.log(Object.keys(list).length);
 await Deno.writeTextFile("files.csv", CSV.stringify(files));
-console.log(cnt); // 30,794,029 (shrink 423,993)
+console.log(cnt); // 30,794,029 (30min shrink 423,993)
+const users = Object.entries(listall);
+await Deno.writeTextFile("listall.csv", CSV.stringify(users));
+console.log("n_users:", Object.keys(listall).length); // 3651
